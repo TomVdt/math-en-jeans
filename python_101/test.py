4bf8a7aaa475debeb0af20da0eed310ed558607f
TomVdt <tomtommy135@gmail.com>
2021-10-12 21:02:10 +0200

    progress on speedrunning python's syntax

--- python_101/test.py
+++ python_101/test.py
@@ -1,13 +1,31 @@
 import numpy as np
 
 
-grid = np.zeros((3, 3), dtype=int)
-turn = 1
+def check_victory(grid):
+	for i in range(3):
+		if grid[i, 0] == grid[i, 1] == grid[i, 2]:
+			return grid[i, 0]
+	return 0
 
-print(grid)
 
-case = int(input("truc"))
+def main():
+	# Initialisation
+	grid = np.zeros((3, 3), dtype=int)
+	turn = 1
+	v = 0
+	game = True
 
-grid[(case-1)//3, (case-1)%3] = turn
+	# Main loop
+	while game:
+		print(grid)
+		case = int(input(f"Au tour du joueur {1 if turn == 1 else 2}:\n"))
+		grid[(case-1)//3, (case-1)%3] = turn
+		v = check_victory(grid)
+		if v != 0:
+			game = False
+		turn *= -1
+	print(v)
 
-print(grid)
+
+if __name__ == '__main__':
+	main()
